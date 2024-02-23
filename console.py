34c9581d4daed4155eb99cdbb3c76e38fe799f51
Davies Magare <davieskamanda@gmail.com>
2024-02-23 15:41:11 +0000

    Task 2

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,10 +118,34 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
+        class_name = args.partition(" ")[0]
+        if class_name not in HBNBCommand.classes:
+            print("**class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        input_params = args.partition(" ")[2].split(' ')
+        new_dict = {}
+        if len(input_params) > 0:
+            for param in input_params:
+                key, value = param.split('=')
+                if '"' in value:
+                    value = value.replace('"', '')
+                    if '_' in value:
+                        value = value.replace('_', ' ')
+                    new_dict[key] = value
+                elif value.isnumeric() or \
+                ('.' in value and value.replace('.', '').isnumeric()):
+                    if '.' in value:
+                        value = float(value)
+                    else:
+                        value = int(value)
+                    new_dict[key] = value
+                else:
+                        continue
+        new_instance = HBNBCommand.classes[class_name]()
+        obj_dict = BaseModel.to_dict(new_instance)
+        obj_dict.update(new_dict)
+        new_instance = HBNBCommand.classes[class_name](**obj_dict)
+        storage.new(new_instance)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +296,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +304,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
