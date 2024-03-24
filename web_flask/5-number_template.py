d1e6cc5fc4e11f44d287878a32f3332494a8857e
Davies Magare <davieskamanda@gmail.com>
2024-03-24 11:29:54 +0000

    added an import to render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Hello hbnb route and view function"""
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.config['strict_slashes'] = False
 
