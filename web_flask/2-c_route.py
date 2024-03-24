19d331cefbf49e6d799b78c07ca444a9af3de1a3
Davies Magare <davieskamanda@gmail.com>
2024-03-24 06:06:33 +0000

    Task 2: What is C

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,5 +17,13 @@ def hello_hbnb_2():
     return 'HBNB'
 
 
+@app.route('/c/<text>')
+def what_is_c(text):
+    """Define route for /c/<text"""
+    if '_' in text:
+        text.replace('_', ' ')
+    return "C {}".format(text)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
