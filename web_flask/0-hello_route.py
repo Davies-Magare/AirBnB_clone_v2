0a2681d5802264a2f0856f3e3ef4eb77916cdf58
Davies Magare <davieskamanda@gmail.com>
2024-03-24 05:21:14 +0000

    Start Flask application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Hello hbnb route and view function"""
+from flask import Flask
+app = Flask(__name__)
+app.config['strict_slashes'] = False
+
+@app.route('/')
+def hello_hbnb():
+    """Display greeting"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
