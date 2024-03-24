14e122ab82d996de1ebc04ca87fa88e0e921e05f
Davies Magare <davieskamanda@gmail.com>
2024-03-24 05:25:12 +0000

    Fixed pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 app.config['strict_slashes'] = False
 
+
 @app.route('/')
 def hello_hbnb():
     """Display greeting"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
