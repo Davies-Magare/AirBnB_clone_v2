1473d09048d7879c827826120ee1f9338bb1c9fc
Davies Magare <Davieskamanda@gmail.com>
2024-04-05 13:09:34 +0000

    Task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Create and Distribute archive to remote web servers."""
+
+from fabric.api import env, put, run
+import os
+import importlib
+
+do_deploy = importlib.import_module('2-do_deploy_web_static').do_deploy
+do_pack = importlib.import_module('1-pack_web_static').do_pack
+
+env.user = 'ubuntu'
+env.key_filename = "/home/vagrant/.ssh/school"
+env.hosts = ['34.224.62.243', '54.90.55.235']  # Define multiple hosts
+
+
+def deploy():
+    """Create and distribute the archive to webservers."""
+    path = do_pack()
+    if path is None:
+        return False
+    success_deploy = do_deploy(path)
+    return success_deploy
