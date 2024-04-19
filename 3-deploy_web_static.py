42494a1a7907951cac692004a4a0f40f3a589940
Davies Magare <Davieskamanda@gmail.com>
2024-04-19 23:03:40 +0000

    Task 0

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,15 +8,16 @@ import importlib
 do_deploy = importlib.import_module('2-do_deploy_web_static').do_deploy
 do_pack = importlib.import_module('1-pack_web_static').do_pack
 
-env.user = 'ubuntu'
-env.key_filename = "/home/vagrant/.ssh/school"
-env.hosts = ['34.224.62.243', '54.90.55.235']  # Define multiple hosts
-
 
 def deploy():
     """Create and distribute the archive to webservers."""
     path = do_pack()
     if path is None:
         return False
+
+    env.user = 'ubuntu'
+    env.key_filename = "/home/vagrant/.ssh/school"
+    env.hosts = ['34.224.62.243', '54.90.55.235']  # Define multiple hosts
+
     success_deploy = do_deploy(path)
     return success_deploy
