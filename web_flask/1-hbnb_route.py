7643db73472879721a385becbaa0f93fdaf1466f
Davies Magare <Davieskamanda@gmail.com>
2024-04-19 23:38:21 +0000

    Task 1: define hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Start a flask application
+and greet the user.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Greet the user."""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display HBNB."""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
